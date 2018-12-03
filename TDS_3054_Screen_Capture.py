522f78af1431bcca5fd3ca798e9e1807b3f56e1b
Jarvis Hill <hilljarvis@hotmail.com>
2018-12-03 15:07:39 -0600

    Update TDS_3054_Screen_Capture.py

--- TDS_3054_Screen_Capture.py
+++ TDS_3054_Screen_Capture.py
@@ -23,12 +23,12 @@ data_file_str = "data.txt"
 
 ##Reads response from instrument
 def read_data(instr):
-    line = ''										##Define empty string
-    while True:										##Read characters until '\n' EOL terminator is received
+    line = ''			##Define empty string
+    while True:			##Read characters until '\n' EOL terminator is received
         ch = instr.read()
-        line +=ch									##Add character to string
+        line +=ch		##Add character to string
         if ch == '\n': break
-    #print line										##Print received data for debbuging
+    #print line			##Print received data for debbuging
     return line
 
 
@@ -80,16 +80,16 @@ def ACQ_WFM(instr):
 	#Configures and retrieves waveform data from all scope channels listed in global list "act_probe_chs"
     for ch in act_probe_chs:
         #Set how measurement data is encoded
-        instr.write("DATa:SOUrce CH"+ch+EOL)                     ##Set CH data source
-        instr.write("DATa:ENCDG ASCIi"+EOL)                      ##Set encoding of CH acquired/measurement data
-        instr.write("DATA:WIDth 1"+EOL)                          ##Set data width (1 = 1 byte per data point)
+        instr.write("DATa:SOUrce CH"+ch+EOL)                    ##Set CH data source
+        instr.write("DATa:ENCDG ASCIi"+EOL)                     ##Set encoding of CH acquired/measurement data
+        instr.write("DATA:WIDth 1"+EOL)                         ##Set data width (1 = 1 byte per data point)
 		
-		#Set data buffer size to retrieve when a wfm request is made (e.g. "CURVe?" cmd)  
-        instr.write("DATA:START 1"+EOL)							 ##Set start index of measurement buffer data	
-        instr.write("DATA:STOP 100"+EOL)						 ##Set stop index of measurement buffer data		
+	#Set data buffer size to retrieve when a wfm request is made (e.g. "CURVe?" cmd)  
+        instr.write("DATA:START 1"+EOL)				##Set start index of measurement buffer data	
+        instr.write("DATA:STOP 100"+EOL)			##Set stop index of measurement buffer data		
         time.sleep(0.1)
 
-		#Request X&Y constants (these are needed to convert the digital data in the instrument buffer into analog values)
+	#Request X&Y constants (these are needed to convert the digital data in the instrument buffer into analog values)
         instr.write("WFMPre:YZEro?"+EOL)
         YZE = float(read_data(instr))
 
@@ -102,19 +102,19 @@ def ACQ_WFM(instr):
     ##  print("%f %f %f" % (YZE, YMU, YOF))                   	##Print waveform preamble params for DEBUG
        
         #Request waveform from a specified CH
-        instr.write("CURVe?"+EOL)								##Send waveforem acquisition request to instrument
+        instr.write("CURVe?"+EOL)				##Send waveforem acquisition request to instrument
         wfm_str = read_data(instr)
         wfm_data = wfm_str[:-1].split(',')                      ##Remove the EOL at the end of the returned wfm string, parses the comma 
-																##delimited string for each data point and stores all points/wfm measurements in a list of strings    
+								##delimited string for each data point and stores all points/wfm measurements in a list of strings    
 
         ##Convert digital(ASCII) encoded scope wfm data into analog voltages
         for point in wfm_data:
-            temp_data = int(point)								##Cast string wfm data point into an integer 							
+            temp_data = int(point)				##Cast string wfm data point into an integer 							
             voltage = YZE + (YMU*(temp_data-YOF))
             if count == 1:
-                voltage_data1.append(voltage)					##Store analog data in a list of floats
+                voltage_data1.append(voltage)			##Store analog data in a list of floats
             elif count == 2:
-                voltage_data2.append(voltage)					##Store analog data in a list of floats
+                voltage_data2.append(voltage)			##Store analog data in a list of floats
 
         count += 1
         
@@ -125,9 +125,9 @@ def ACQ_WFM(instr):
 ##    print(len(voltage_data2))
 
     ##Write data to file
-    f = open(data_file_str,'w')													##Open file in working directory (place where Python script lives)
-    f.write("Time[s]\tWFM1 Voltage[V]\tWFM2 Voltage[V]\n")						##Write file header
-    for i in range(0,len(voltage_data1)):										##Write data from all active channels to the file
+    f = open(data_file_str,'w')					##Open file in working directory (place where Python script lives)
+    f.write("Time[s]\tWFM1 Voltage[V]\tWFM2 Voltage[V]\n")	##Write file header
+    for i in range(0,len(voltage_data1)):			##Write data from all active channels to the file
         f.write("\t"+str(voltage_data1[i])+"\t"+str(voltage_data2[i])+"\n")		
 
     f.close()																	##Close file
@@ -140,13 +140,13 @@ def main():
     #Connect with TDS3000 series oscilloscope 
     scope = Config_serial_comms()
     
-	#Configure instrument
-	Config_instr(scope)
+    #Configure instrument
+    Config_instr(scope)
 	
-	#Acquire waveform
+    #Acquire waveform
     ACQ_WFM(scope)
 
-	#Close instrument connection
+    #Close instrument connection
     scope.close()
 
 
